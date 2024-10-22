91b637eb6c2d8450802a32f297406e489712c074
Susmoy Debnath <susmoy1025@gmail.com>
2024-10-22 12:14:30 +0600

    third commit

--- src/components/About components/Education.js
+++ src/components/About components/Education.js
@@ -31,15 +31,15 @@ export default function Education() {
 
                 </div>
                 <div className='m-4 text-justify lg:w-1/2'>
-                    <img className='rounded-xl mb-4 lg:w-1/2' src="/pallima.jpg" alt="" />
-                    <h1 className='text-2xl font-Quicksand font-bold border-b-2 border-neutral-400 pb-1 mb-4'>School</h1>
-                    <p className='font-Inter'>Shahid Babul Academy is an educational wing of the prestigious social organization Pallima Sangsad of Khilgaon Residential Area, Dhaka. It is named after the freedom fighter Shahid Amirus Salam Babul who fought alongside Shahid Abdullah-Hel-Baki Bir Pratik. The school was established on 15 January, 1976. There is also a rich library, Shahid Baki Smrity Pathagar named after Shahid Abdullah-Hel-Baki Bir Pratik.</p>
-
-                </div>
-                <div className='m-4 text-justify lg:w-1/2'>
                     <img className='rounded-xl mb-4 lg:w-1/2' src="/frii.jpg" alt="" />
                     <h1 className='text-2xl font-Quicksand font-bold border-b-2 border-neutral-400 pb-1 mb-4'>High School</h1>
                     <p className='font-Inter'>Faizur Rahman Ideal Institute is an educational institute situated in Dhaka, Bangladesh. It has three campuses. It was founded in 1998 by Md Faizur Rahman, an eminent educationist and former founder and Principal of Motijheel Ideal School and College, Dhaka with a mission to introduce the students to their Creator as well as to provide modern education on the basis of religious context for making them worthy citizens.</p>
+
+                </div>
+                <div className='m-4 text-justify lg:w-1/2'>
+                    <img className='rounded-xl mb-4 lg:w-1/2' src="/pallima.jpg" alt="" />
+                    <h1 className='text-2xl font-Quicksand font-bold border-b-2 border-neutral-400 pb-1 mb-4'>School</h1>
+                    <p className='font-Inter'>Shahid Babul Academy is an educational wing of the prestigious social organization Pallima Sangsad of Khilgaon Residential Area, Dhaka. It is named after the freedom fighter Shahid Amirus Salam Babul who fought alongside Shahid Abdullah-Hel-Baki Bir Pratik. The school was established on 15 January, 1976. There is also a rich library, Shahid Baki Smrity Pathagar named after Shahid Abdullah-Hel-Baki Bir Pratik.</p>
 
                 </div>
             </motion.div>
